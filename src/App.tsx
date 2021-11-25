7c13971994543428182ba65240145d1ee26670be
checker <trinhlehainam@protonmail.com>
2021-11-26 08:29:18 +0900

    Add project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './index.css'
+
+function App() {
+    return (
+    <div className="flex justify-center bg-gray-300">
+        <div className="mx-4 self-center text-center">
+            <h1 className="text-6xl font-bold text-blue-700">Welcome to Checker</h1>
+            <h2 className="text-3xl font-semibold text-blue-300">
+                A premium in the art of porn
+            </h2>
+            <button className="my-4 px-4 py-2 border-2 border-black rounded-lg text-white bg-blue-900">
+                Go premium
+            </button>
+        </div>
+    </div>
+    );
+}
+
+export default App;
