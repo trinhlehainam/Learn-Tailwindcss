f23015a5d68a2abefd2edec49d81d044c59ee718
checker <trinhlehainam@protonmail.com>
2021-11-27 12:30:18 +0900

    move tailwind config file to root

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,12 @@ import "./index.css";
 
 function App() {
   return (
-    <>
-        <div className="flex flex-row flex-wrap">
-            <div className="text-center w-full">Header</div>
-            <div className="text-center w-1/5">Left Sidebar</div>
-            <div className="text-center w-3/5">Content</div>
-            <div className="text-center w-1/5">Right Sidebar</div>
-            <div className="text-center w-full">Footer</div>
+    <div className="bg-zelda-darkGreen min-h-screen pt-32">
+        <div className="container mx-auto flex flex-col xl:flex-row">
+            <div className="w-full xl:w-1/2"></div>
+            <div className="w-full xl:w-1/2"></div>
         </div>
-    </>
+    </div>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                zelda: {
+                    blue: "#0290fe",
+                    darkGreen: "#0c1f0c",
+                    darkGray: "#2f3124",
+                    lightGray: "#616356",
+                    yellow: "#FFC800",
+                    lightYellow: "#FFFFBE",
+                    softYellow: "#f8f7d9",
+                },
+            },
+            boxShadow: {
+                yellow: "rgba(255, 255, 190, 0.4) 0px 0px 6px 2px",
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
