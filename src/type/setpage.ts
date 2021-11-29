93ea996509a8eb1e6930ff3d7d450d86923fb259
Trinh Le Hai Nam <trinhlehainam1607@gmail.com>
2021-11-29 15:16:51 +0900

    Add svg icon in typescript and navigate page utilitiy

---
+++ src/type/setpage.ts
@@ -0,0 +1,5 @@
+import { Dispatch, SetStateAction } from "react";
+
+type SetPage_t = Dispatch<SetStateAction<[number, number]>>
+
+export default SetPage_t;
---
+++ src/utils/navigatePage.ts
@@ -0,0 +1,16 @@
+import SetPage_t from '../type/setpage'
+
+const clamp = (value: number, min: number, max: number) => (
+    Math.min(max, Math.max(min, value))
+);
+
+const navigatePage = (
+    newDirection: number,
+    currentPage: number,
+    setPage: SetPage_t,
+    maxPage = 2
+) => {
+    setPage([clamp(currentPage + newDirection, 0, maxPage), newDirection]);
+};
+
+export default navigatePage;
